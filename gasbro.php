12e31180843673e95aa0fed2506cd21f57051429
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2020-04-29 04:22:05 +0700

    Create gasbro.php

---
+++ gasbro.php
@@ -0,0 +1,35 @@
+<?php
+///////////////////////////////////////////
+///////CREATED BY ARUDJI HERMATYAR////////
+//////www.facebook.com/bantalku567///////
+/////https://github.com/arudji1211//////
+////Fixed by Bintang Nur Pradana///////
+///////////////////////////////////////
+
+include 'tri_req.php';
+
+$tri = new tri();
+$imei = "868880043302499";
+echo "Masukkan No Telepon : ";
+$msisdn = trim(fgets(STDIN));
+$otp = $tri->request_otp($msisdn,$imei);
+echo "This Script Created by Arudji : ";
+echo "This Script Fixed by St4rs : ";
+echo $otp[1] . "\r\n";
+echo "Masukkan OTP : ";
+$otp = trim(fgets(STDIN));
+$login = $tri->login($msisdn,$otp);
+$login = json_decode($login,true);
+$bearer = $login['access_token'];
+$id = $tri->trans($bearer);
+$id = json_decode($id,true);
+$id = $id['data'][0]['rewardTransactionId'];
+for($id1 = 23111802; $id1 < 23111803;$id1++)
+{
+  $gas = $tri->claim($bearer,$id,$id1);
+  echo $gas . "\r\n";
+  sleep(2);
+}
+
+
+?>
