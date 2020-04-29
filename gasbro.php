7df27bc77e6797477e39d8f109a5340aeb08f658
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2020-04-29 09:04:52 +0700

    Update gasbro.php

--- gasbro.php
+++ gasbro.php
@@ -20,8 +20,8 @@ $login = json_decode($login,true);
 $bearer = $login['access_token'];
 $id = $tri->trans($bearer);
 $id = json_decode($id,true);
-$id = $id['data'][0]['rewardTransactionId'];
-for($id1 = 16051901; $id1 < 19061902;$id1++)
+$id = $id['data'][P20042908LRB3856829310]['rewardTransactionId'];
+for($id1 = 16051901; $id1 < 23111802;$id1++)
 {
   $gas = $tri->claim($bearer,$id,$id1);
   echo $gas . "\r\n";
