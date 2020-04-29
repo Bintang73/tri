45bc729974253fa6dc0187c4eabf50aea867a954
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2020-04-29 07:12:39 +0700

    Update gasbro.php

--- gasbro.php
+++ gasbro.php
@@ -8,8 +8,8 @@
 include 'request.php';
 
 $tri = new tri();
-$imei = "868880043302499";
-echo "Masukkan No Telepon : ";
+$imei = "867889005326788";
+echo "st4rs Masukkan No Telepon : ";
 $msisdn = trim(fgets(STDIN));
 $otp = $tri->request_otp($msisdn,$imei);
 echo $otp[1] . "\r\n";
@@ -20,8 +20,8 @@ $login = json_decode($login,true);
 $bearer = $login['access_token'];
 $id = $tri->trans($bearer);
 $id = json_decode($id,true);
-$id = $id['data'][0]['rewardTransactionId'];
-for($id1 = 19061902; $id1 < 23111803;$id1++)
+$id = $id['data'][23111802]['rewardTransactionId'];
+for($id1 = 1500; $id1 < 1600;$id1++)
 {
   $gas = $tri->claim($bearer,$id,$id1);
   echo $gas . "\r\n";
