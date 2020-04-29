76f30b3267f99370c7959029b0832e624315013a
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2020-04-29 08:38:25 +0700

    Update request.php

--- request.php
+++ request.php
@@ -81,7 +81,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
   }
   
   function claim($bearer,$id,$id1)
-  {
+    {
     $body = array("rewardId"=>$id1,"rewardTransactionId"=>$id);
     $body = json_encode($body);
     $ctl = strlen($body);
@@ -93,10 +93,10 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
  "Authorization:Bearer " . $bearer ,
  "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
  "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/tukarpoint" ,
+ "Referer:http://bonstri.tri.co.id/voucherku" ,
  "Accept-Encoding:gzip, deflate" ,
  "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-     $response = $this->curls("http://bonstri.co.id/api/v1/tukar-points/categories",$header,$body,"POST");
+     $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/get-voucher-code",$header,$body,"POST");
      return $response[1];
   }
   
